2f03b3b858fa01eb12fd10eab0d0b8674250fb10
serahan <seyrahan22@gmail.com>
2019-05-31 15:14:31 +0900

    초기 커밋.

---
+++ ClassTest/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTest
+{
+    class Animal
+    {
+        private string name;
+        private string sound;
+
+        public Animal()
+        {
+
+        }
+
+        public Animal(string n, string s)
+        {
+            name = n;
+            sound = s;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("{0} says {1}", name, sound);
+        }
+    }
+}
---
+++ ClassTest/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTest // C#
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Rectangle rect1;
+            //rect1.length = 100;
+            //rect1.width = 30;
+
+            //Console.WriteLine("rectangle length : {0}, width : {1}", rect1.length, rect1.width);
+
+            //Rectangle rect2 = new Rectangle(200, 50);
+            //Console.WriteLine("rectangle2 length : {0}, width : {1}, area : {2}",
+            //    rect2.length, rect2.width, rect2.Area());
+
+            Animal fox = new Animal("fox","Reaww");
+            fox.MakeSound();
+
+            Animal cat = new Animal("cat", "meow");
+            cat.MakeSound();
+
+            Animal dog = new Animal("dog", "waw");
+            dog.MakeSound();
+
+            Console.ReadLine();
+        }
+        
+        struct Rectangle
+        {
+            public double length;
+            public double width;
+
+            public Rectangle(double l, double w)
+            {
+                length = l;
+                width = w;
+            }
+
+            public double Area()
+            {
+                return length * width;
+            }
+        }
+    }
+}
